85a4eebc20d9d5ac9726ee078818f97e7dc66d8c
Alexander Harms <a.g.j.harms@gmail.com>
2020-04-27 19:07:23 +0200

    Removes non-essential scripts and data files

--- temp_folder/prepareCrimeData.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import pandas as pd
-
-# Read in crime data
-crimes = pd.read_csv('./Data/Chicago_Crimes_2012_to_2017.csv')
-crimes = crimes.query('Arrest == True')
-crimes.write_csv("./Data-process/Crimes-Arrested.csv")
--- temp_folder/zipcodes.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import geopandas as gpd
-
-zip_codes = gpd.read_file("Boundaries - ZIP Codes.geojson")
-print(zip_codes.zip.values)
