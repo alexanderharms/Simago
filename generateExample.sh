f0b29ac45e0f1a6be8eb665d1deb7ca0b27cd85c
alexanderharms <a.g.j.harms@gmail.com>
2020-07-19 17:05:58 +0200

    Moves examples to other repository

--- generateExample.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-POPSIZE=1000
-YAML='./data-yaml/example/'
-OUTPUT='./output/population.csv'
-SEED=100
-
-python3 generatePopulation.py -p $POPSIZE --yaml_folder $YAML\
-	-o $OUTPUT --rand_seed $SEED
-
