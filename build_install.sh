5de06f9037163a79e6fd137303fae0045251e12b
Alexander Harms <a.g.j.harms@gmail.com>
2020-10-23 13:27:06 +0200

    Adds .readthedocs.yml, and aesthetic fixes to pyproject.toml

--- build_install.sh
+++ build_install.sh
@@ -4,5 +4,5 @@ make html
 cd ..
 python3 -m pep517.build .
 
-pip3 uninstall simago
-pip3 install ./dist/simago-0.2.0-py3-none-any.whl
+# pip3 uninstall simago
+# pip3 install ./dist/simago-0.2.0-py3-none-any.whl
