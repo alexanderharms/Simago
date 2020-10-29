29240ca4d549e0a62beb9ac0362c56c481c7adbd
Alexander Harms <a.g.j.harms@gmail.com>
2020-10-29 16:43:47 +0100

    Adds example again

---
+++ example/conversion_scripts/generate_sex_csv.py
@@ -0,0 +1,19 @@
+"""
+Converts data about the sex of the population from www.populationpyramid.net
+to the simago format.
+"""
+import pandas as pd
+
+
+data_frame = pd.read_csv("./data/WORLD-2017-adapted.csv")
+male_value = data_frame['M'].sum()
+female_value = data_frame['F'].sum()
+
+data_export = pd.DataFrame(
+        data={"option": [0, 1],
+              "value": [male_value, female_value],
+              "label": ["male", "female"],
+              "conditional_index": 0})
+print(data_export)
+
+data_export.to_csv("./data/example/sex.csv", index=False)
